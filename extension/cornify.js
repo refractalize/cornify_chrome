92f4f1163e745d1f107ed0ef9e4bff0f06f36da0
Tim Macfarlane <timmacfarlane@gmail.com>
2010-03-25 23:33:53 +0000

    commit of vers 10001

---
+++ extension/cornify.js
@@ -0,0 +1,133 @@
+var cornify = function () {
+    console.log("cornifying!");
+    var d = document,
+        j = d.getElementById('__cornify_nodes'),
+        k = null;
+        
+    if (!j) {
+        k = d.createElement('div');
+        k.id = '__cornify_nodes';
+        d.getElementsByTagName('body')[0].appendChild(k);
+    }
+    
+    cornify_add();
+};
+
+var cornify_count = 0;
+cornify_add = function() {
+  cornify_count += 1;
+  var cornify_url = 'http://www.cornify.com/';
+  var div = document.createElement('div');
+  div.style.position = 'fixed';
+  
+  var numType = 'px';
+  var heightRandom = Math.random()*.75;
+  var windowHeight = 768;
+  var windowWidth = 1024;
+  var height = 0;
+  var width = 0;
+  var de = document.documentElement;
+  if (typeof(window.innerHeight) == 'number') {
+    windowHeight = window.innerHeight;
+    windowWidth = window.innerWidth;
+  } else if(de && de.clientHeight) {
+    windowHeight = de.clientHeight;
+    windowWidth = de.clientWidth;
+  } else {
+    numType = '%';
+    height = Math.round( height*100 )+'%';
+  }
+  
+  div.onclick = cornify_add;
+  div.style.zIndex = 10;
+  div.style.outline = 0;
+  
+  if( cornify_count==15 ) {
+    div.style.top = Math.max( 0, Math.round( (windowHeight-530)/2 ) )  + 'px';
+    div.style.left = Math.round( (windowWidth-530)/2 ) + 'px';
+    div.style.zIndex = 1000;
+  } else {
+    if( numType=='px' ) div.style.top = Math.round( windowHeight*heightRandom ) + numType;
+    else div.style.top = height;
+    div.style.left = Math.round( Math.random()*90 ) + '%';
+  }
+  
+  var img = document.createElement('img');
+  var currentTime = new Date();
+  var submitTime = currentTime.getTime();
+  if( cornify_count==15 ) submitTime = 0;
+  img.setAttribute('src',cornify_url+'getacorn.php?r=' + submitTime + '&url='+document.location.href);
+  var body = document.getElementsByTagName('body')[0];
+  body.appendChild(div);
+  div.appendChild(img);
+  
+  // Add stylesheet.
+  if (cornify_count == 5) {
+    var cssExisting = document.getElementById('__cornify_css');
+    if (!cssExisting) {
+      var head = document.getElementsByTagName("head")[0];
+      var css = document.createElement('link');
+      css.id = '__cornify_css';
+      css.type = 'text/css';
+      css.rel = 'stylesheet';
+      css.href = 'http://www.cornify.com/css/cornify.css';
+      css.media = 'screen';
+      head.appendChild(css);
+    }
+    cornify_replace();
+  } 
+}
+
+cornify_replace = function() {
+  // Replace text.
+  var hc = 6;
+  var hs;
+  var h;
+  var k;
+  var words = ['Happy','Sparkly','Glittery','Fun','Magical','Lovely','Cute','Charming','Amazing','Wonderful'];
+  while(hc >= 1) {
+    hs = document.getElementsByTagName('h' + hc);
+    for (k = 0; k < hs.length; k++) {
+      h = hs[k];
+      h.innerHTML = words[Math.floor(Math.random()*words.length)] + ' ' + h.innerHTML;
+    }
+    hc-=1;
+  }
+}
+
+/*
+ * Adapted from http://www.snaptortoise.com/konami-js/
+ */
+var cornami = {
+  input:"",
+  pattern:"38384040373937396665",
+  clear:setTimeout('cornami.clear_input()',5000),
+  load: function() {
+    window.document.onkeydown = function(e) {
+      if (cornami.input == cornami.pattern) {
+        cornify_add();
+        clearTimeout(cornami.clear);
+        return;
+      }
+      else {
+        cornami.input += e ? e.keyCode : event.keyCode;
+        if (cornami.input == cornami.pattern) cornify_add();
+        clearTimeout(cornami.clear);
+        cornami.clear = setTimeout("cornami.clear_input()", 5000);
+      }
+    }
+  },
+  clear_input: function() {
+    cornami.input="";
+    clearTimeout(cornami.clear);
+  }
+}
+cornami.load();
+
+chrome.extension.onRequest.addListener(
+  function(request, sender, sendResponse) {
+      cornify();
+      sendResponse({}); // snub them.
+  });
+
+//setInterval(cornify, 2000);
